5a6a9c9a8af59041a7d80afe0d02664d9c4f952f
Kyle Dodge <dodge.kyle@gmail.com>
2015-01-24 01:28:52 -0800

    Added comments

--- www/app/TimeUtil.js
+++ www/app/TimeUtil.js
@@ -1,12 +1,21 @@
 define(function () {
     "use strict";
 
+    /***
+     * Time utility module to provide functions for dealing with time and color as time
+     * @constructor
+     */
     function Time() {
-
     }
 
     Time.prototype = {
 
+        /***
+         * Retrieves the hour from a date object, formatted to 12 or 24 hour format
+         * @param {date} time - The date object to get the hour from
+         * @param {boolean} is24Hour - Flag for returning 12 or 24 hour format
+         * @returns {number}
+         */
         currentHour: function (time, is24Hour) {
             var hours = time.getHours();
 
@@ -17,22 +26,47 @@ define(function () {
             return this.padWithZero(hours);
         },
 
+        /***
+         * Retrieves the minutes from a date object
+         * @param {date} time - The date object to get the minutes from
+         * @returns {number}
+         */
         currentMinute: function (time) {
             return this.padWithZero(time.getMinutes());
         },
 
+        /***
+         * Retrieves the seconds from a date object
+         * @param {date} time - The date object to get the seconds from
+         * @returns {number}
+         */
         currentSecond: function (time) {
             return this.padWithZero(time.getSeconds());
         },
 
+        /***
+         * Determines if current time is AM or PM
+         * @param {date} time - The date object to get the time string from
+         * @returns {string} - Possible results are 'AM' or 'PM'
+         */
         currentAMPM: function (time) {
             return time.getHours() < 12 ? "AM" : "PM";
         },
 
+        /***
+         * Helper function to make sure all numbers are 2 digit values. If one digit, will pad with a zero.
+         * @param {number} value - The number value to check
+         * @returns {string}
+         */
         padWithZero: function (value) {
             return (value < 10 ? "0" : "") + value;
         },
 
+        /***
+         * Retrieves the hh:mm:ss:AM/PM time string from a date object
+         * @param {date} time - The date object to get the full time string from
+         * @returns {string}
+         */
         getTimeString: function (time) {
             return this.currentHour(time) + ":" +
                 this.currentMinute(time) + ":" +
@@ -40,12 +74,22 @@ define(function () {
                 this.currentAMPM(time);
         },
 
+        /***
+         * Retrieves the current time as a hexidecimal string with a # prepended.
+         * @param {date} time - The date object to get the hexidecimal time string from
+         * @returns {string}
+         */
         getTimeHex: function (time) {
             return "#" + this.currentHour(time) +
                 this.currentMinute(time) +
                 this.currentSecond(time);
         },
 
+        /***
+         * Retrieves the current time as a rgb string.
+         * @param {date} time - The date object to get the rgb time string from
+         * @returns {string}
+         */
         getTimeRGB: function (time) {
             var timeStr = this.getTimeHex(time);
             var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
--- www/app/main.js
+++ www/app/main.js
@@ -1,5 +1,8 @@
 define([
     'app/Clock'
 ], function (Clock) {
+
+    //initialize the clock once it has been loaded
     var clock = new Clock();
+
 });
